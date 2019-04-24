a51d5b79d0940232e2ba1fed884f8da6b29d7ef8
jdansey <jdansey@bu.edu>
2019-04-23 20:27:37 -0400

    fix sqlite path

--- civic_pulse_app/settings.py
+++ civic_pulse_app/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'civic_pulse_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../PycharmProjects/db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
