3120cb156f184b9438c29a84716d3b34176af0cf
Jacob Dansey <jdansey@bu.edu>
2019-04-23 20:46:57 -0400

    Add Agency Fields (#16)
    * keep swapping hard code for variables
    
    * fix staticfiles
    
    * make google api key environment variable
    
    * remove todo
    
    * add agency fields
    
    * fix sqlite path

--- civic_pulse/models.py
+++ civic_pulse/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 
 
+def logo_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/agency_logos/<id>/<filename>
+    return 'agency_logos/{0}/{1}'.format(instance.id, filename)
+
+
 # Create your models here.
 class Agency(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -11,6 +16,15 @@ class Agency(models.Model):
     twitter = models.CharField(max_length=100,blank=True)
     facebook = models.CharField(max_length=100,blank=True)
     phone_number = models.CharField(max_length=15,blank=True)
+    address = models.CharField(max_length=250,blank=True)
+    description = models.CharField(max_length=1000,blank=True)
+    last_successful_scrape = models.DateTimeField(blank=True,null=True)
+    scrape_counter = models.IntegerField(default=0)
+    logo = models.ImageField(upload_to=logo_path, blank=True)
+
+    # Geolocation
+    latitude = models.DecimalField(max_digits=8, decimal_places=3,default=0)
+    longitude = models.DecimalField(max_digits=8, decimal_places=3,default=0)
 
     def __str__(self):
         return self.name
