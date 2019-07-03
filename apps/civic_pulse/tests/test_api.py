80c40bc5a0ca59a8fcdf21fb6f6faf7a3f136416
jdansey <jdansey@bu.edu>
2019-07-03 14:54:00 -0400

    add entry api tests

--- apps/civic_pulse/tests/test_api.py
+++ apps/civic_pulse/tests/test_api.py
@@ -22,7 +22,7 @@ class AgencyAPITest(TestCase):
         self.assertEqual(200, response.status_code)
 
         agencies_json = json.loads(response.content.decode("utf-8"))
-        expected_return = [{
+        expected_results = [{
             'id': 1,
             'name': 'Test Agency 1',
             'website': '',
@@ -45,14 +45,14 @@ class AgencyAPITest(TestCase):
             'last_successful_scrape': None,
             'scrape_counter': 0
         }]
-        self.assertEqual(agencies_json,expected_return)
+        self.assertEqual(agencies_json,expected_results)
 
     def test_GET_Individual(self):
         response = self.client.get('/api/agencies/1/')
         self.assertEqual(200, response.status_code)
 
         agency_json = json.loads(response.content.decode("utf-8"))
-        expected_return = {
+        expected_results = {
             'id': 1,
             'name': 'Test Agency 1',
             'website': '',
@@ -64,7 +64,7 @@ class AgencyAPITest(TestCase):
             'last_successful_scrape': None,
             'scrape_counter': 0
         }
-        self.assertEqual(agency_json, expected_return)
+        self.assertEqual(agency_json, expected_results)
 
     def test_POST_Unauthorized(self):
         data = {
@@ -106,3 +106,111 @@ class AgencyAPITest(TestCase):
         self.assertEqual(json_response, expected_results)
 
 
+class EntryAPITest(TestCase):
+
+    def setUp(self):
+        self.agency = Agency.objects.create(
+            name='Test Agency 1',
+            id=1
+        )
+        Entry.objects.create(
+            agency_id=self.agency.id,
+        )
+        Entry.objects.create(
+            agency_id=self.agency.id,
+            https_enabled=True,
+        )
+
+        self.client = APIClient()
+
+    def test_GET(self):
+        response = self.client.get('/api/entries/')
+        self.assertEqual(200, response.status_code)
+
+        entries_json = json.loads(response.content.decode("utf-8"))
+        expected_results = [{
+            'id': 1,
+            'agency': 1,
+            'https_enabled': False,
+            'has_privacy_policy': False,
+            'mobile_friendly': False,
+            'good_performance': False,
+            'has_social_media': False,
+            'has_contact_info': False
+        }, {
+            'id': 2,
+            'agency': 1,
+            'https_enabled': True,
+            'has_privacy_policy': False,
+            'mobile_friendly': False,
+            'good_performance': False,
+            'has_social_media': False,
+            'has_contact_info': False
+        }]
+
+        self.assertEqual(entries_json,expected_results)
+
+    def test_GET_Individual(self):
+        response = self.client.get('/api/entries/1/')
+        self.assertEqual(200, response.status_code)
+
+        entry_json = json.loads(response.content.decode("utf-8"))
+        expected_results = {
+            'id': 1,
+            'agency': 1,
+            'https_enabled': False,
+            'has_privacy_policy': False,
+            'mobile_friendly': False,
+            'good_performance': False,
+            'has_social_media': False,
+            'has_contact_info': False
+        }
+
+        self.assertEqual(entry_json, expected_results)
+
+    def test_POST_Unauthorized(self):
+        data = {
+            'agency': 1,
+            'https_enabled': True,
+            'has_privacy_policy': False,
+            'mobile_friendly': False,
+            'good_performance': False,
+            'has_social_media': True,
+            'has_contact_info': False
+        }
+        response = self.client.post('/api/entries/',data=data,format='json')
+        self.assertEqual(401,response.status_code)
+
+        json_response = json.loads(response.content.decode("utf-8"))
+        self.assertEqual("Authentication credentials were not provided.",json_response['detail'])
+
+    def test_POST_Authorized(self):
+        user = User.objects.create_user(username='test',email='test@test.test',password='test')
+        token = Token.objects.create(user=user)
+
+        data = {
+            'agency': 1,
+            'https_enabled': True,
+            'has_privacy_policy': False,
+            'mobile_friendly': False,
+            'good_performance': False,
+            'has_social_media': True,
+            'has_contact_info': False
+        }
+
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+        response = self.client.post('/api/entries/', data=data, format='json')
+        self.assertEqual(201, response.status_code)
+
+        json_response = json.loads(response.content.decode("utf-8"))
+        expected_results = {
+            'id': 3,
+            'agency': 1,
+            'https_enabled': True,
+            'has_privacy_policy': False,
+            'mobile_friendly': False,
+            'good_performance': False,
+            'has_social_media': True,
+            'has_contact_info': False
+        }
+        self.assertEqual(json_response, expected_results)
