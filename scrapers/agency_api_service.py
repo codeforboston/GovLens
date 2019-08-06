2428a1832c05d922543ebe1de68da1acb2a0da8c
djp9327 <daniel@Daniels-MacBook-Pro.local>
2019-08-06 13:11:18 -0700

    removing comments/repeated code

--- scrapers/agency_api_service.py
+++ scrapers/agency_api_service.py
@@ -7,12 +7,7 @@ class AgencyApiService:
 
     def get_all_agencies(self):
         try:
-            agency_list = self._get(self.base_url)
-            # I was not able to find a way to get the count of the agencies. so first making a call to get the count and then passing the count to get all the agencies
-            # Instead of getting everything, get the agencies 20 at a time (pagesize)
-            agency_count = len(agency_list)
-            all_agency_url = f"{self.base_url}?limit={agency_count}"
-            all_agency_list = self._get(all_agency_url)
+            all_agency_list = self._get(self.base_url)
             return all_agency_list
         except Exception as ex:
             print(f"Error while retrieving all the agency information: {str(ex)}")
@@ -25,6 +20,5 @@ if __name__=="__main__":
     svc = AgencyApiService()
     agens = svc.get_all_agencies()
     scraped = scrape_data(agens)
-    import pdb;pdb.set_trace()
     print ("SCRAPED")
 
