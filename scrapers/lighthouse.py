296439e19d1bb2f6d0a15dc983b6dad8611f9f61
dpere <djp9327@gmail.com>
2019-08-10 19:58:22 -0500

    adding separate clients for page insights and mobile friendly

--- scrapers/lighthouse.py
+++ scrapers/lighthouse.py
@@ -1,11 +1,14 @@
-import os
+import os, time
+import asyncio, aiofiles, aiohttp
 import requests, json
 from django.conf import settings
 from scrapers.base_api_client import ApiClient
+from agency_api_service import AgencyApiService
 
-GOOGLE_API_KEY = "AIzaSyCJOEyfcXBfnLt3dpaUAD78Pp8XfIbGSx0" #os.environ['GOOGLE_API_KEY']
+
+GOOGLE_API_KEY = ""  #os.environ['GOOGLE_API_KEY']
 PAGE_INSIGHTS_ENDPOINT = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed"
-MOBILE_FRIENDLY_ENDPOINT = "https://search.google.com/test/mobile-friendly" # from what i have tested, very hard to automate
+MOBILE_FRIENDLY_ENDPOINT = "https://searchconsole.googleapis.com/v1/urlTestingTools/mobileFriendlyTest:run" # from what i have tested, very hard to automate
 
 '''
 Lighthouse has 5 categories of information that can be pulled from a url
@@ -20,20 +23,27 @@ class PageInsightsClient(ApiClient):
     def __init__(self, api_uri=PAGE_INSIGHTS_ENDPOINT, api_key=GOOGLE_API_KEY):
         ApiClient.__init__(self, api_uri, api_key)
 
-    def get_page_insights(self, url):
+    def get_page_insights(self, url, category):
         data = {
                 'url': url,
-                'key': self.api_key
+                'key': self.api_key,
+                'category': category
         }
         return self.get("", data=data)
 
-
-if __name__=="__main__":
-    google_client = PageInsightsClient(PAGE_INSIGHTS_ENDPOINT, GOOGLE_API_KEY)
-    data = {'url':"https://stackoverflow.com/questions/24022558/differences-between-staticfiles-dir-static-root-and-media-root"}
-    params = {'key':google_client.api_key}
-
-    response = google_client.get_page_insights("https://github.com/codeforboston/civicpulse/pull/50/commits/98f1c32bd3e8798d0fe3d47f68d05744c47ff275")
-    print("CHECKED")
+class GoogleMobileFriendlyClient(ApiClient):
+    def __init__(self, api_uri=MOBILE_FRIENDLY_ENDPOINT, api_key=GOOGLE_API_KEY):
+        self.urls = []
+        self.results = []
+        ApiClient.__init__(self, api_uri, api_key)
+        
+    def get_mobile_friendly(self, url, index):
+        data = {
+                'url': url
+        }
+        params = {
+                'key': self.api_key
+        }
+        return self.post("", index, data=data, params=params)
 
 
