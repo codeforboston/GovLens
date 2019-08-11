8813da87571db27beb6cd13f345050ddc4300143
dpere <djp9327@gmail.com>
2019-08-10 19:56:00 -0500

    refactoring scraper logic

--- scrapers/scrapers/accessibility_scraper.py
+++ scrapers/scrapers/accessibility_scraper.py
@@ -1,4 +1,3 @@
-import requests, os, json
 from .base_scraper import BaseScraper
 from agency_dataaccessor import AgencyDataAccessor
 from .base_api_client import ApiClient
@@ -12,12 +11,10 @@ class AccessibilityScraper(BaseScraper):
         self.apiClient = PageInsightsClient()
 
     def get_website_accessibility_info(self):
-        insights = self.apiClient.get_page_insights(self.url)
-        lighthouse_results = insights['lighthouseResult']
         return {
-            "mobile_friendly": self.get_mobile_friendliness(lighthouse_results),
-            "page_speed": self.get_page_speed(lighthouse_results),
-            "performance": self.get_site_performance(lighthouse_results),
+            "mobile_friendly": self.get_mobile_friendliness(),
+            "page_speed": self.get_page_speed(),
+            "performance": self.get_site_performance(),
             "multi_lingual": self.get_multi_lingual()
         }
 
@@ -26,16 +23,18 @@ class AccessibilityScraper(BaseScraper):
         or ("espanol" or "Espa&ntilde;ol") in self.page.a) else False
         return self.get_criteria_object(None, is_criteria_met)
 
-    def get_site_performance(self, lighthouse_results):
+    def get_site_performance(self):
         try:
+            lighthouse_results = self.apiClient.get_page_insights(self.url, 'performance').content['lighthouseResult']
             performanceResults = lighthouse_results['categories']['performance']['score']
             is_criteria_met = True if performanceResults*100 >= 80 else False # the score in the Json file is a percentage
             return self.get_criteria_object(performanceResults, is_criteria_met)
         except:
             print("Error in get_site_performance for", self.url)
 
-    def get_mobile_friendliness(self, lighthouse_results):
+    def get_mobile_friendliness(self):
         try:
+            lighthouse_results = self.apiClient.get_page_insights(self.url, 'pwa').content['lighthouseResult']
             #If the width of your app's content doesn't match the width of the viewport, your app might not be optimized for mobile screens.
             score = lighthouse_results['audits']['content-width']['score']
             title = lighthouse_results['audits']['content-width']['title']
@@ -44,8 +43,9 @@ class AccessibilityScraper(BaseScraper):
         except:
             print("Error in get_mobile_friendliness for", self.url)
 
-    def get_page_speed(self, lighthouse_results):
+    def get_page_speed(self):
         try:
+            lighthouse_results = self.apiClient.get_page_insights(self.url, 'performance').content['lighthouseResult']
             speed_index = lighthouse_results['audits']['speed-index']['score']
             is_criteria_met = True if speed_index*100 >= 80 else False # the score in the Json file is a percentage
             return self.get_criteria_object(speed_index, is_criteria_met)
--- scrapers/scrapers/security_scraper.py
+++ scrapers/scrapers/security_scraper.py
@@ -12,24 +12,24 @@ class SecurityScraper(BaseScraper):
         self.apiClient = PageInsightsClient()
 
     def get_security_privacy_info(self):
-        insights = self.apiClient.get_page_insights(self.url)
-        lighthouse_results = insights['lighthouseResult']
         return {
-            "https": self.get_http_acess(lighthouse_results),
-            "hsts": self.get_hsts(lighthouse_results),
+            "https": self.get_http_acess(),
+            "hsts": self.get_hsts(),
             "privacy_policies": self.get_privacy_policies()
         }
 
-    def get_http_acess(self, lighthouse_results):
+    def get_http_acess(self):
         try:
+            lighthouse_results = self.apiClient.get_page_insights(self.url, 'pwa').content['lighthouseResult']
             score = lighthouse_results['audits']['is-on-https']['score']
             is_criteria_met = True if score == 1 else False
             return self.get_criteria_object(score, is_criteria_met)
         except:
             print("Error in get_http_acess for", self.url)
 
-    def get_hsts(self, lighthouse_results):
+    def get_hsts(self):
         try:
+            lighthouse_results = self.apiClient.get_page_insights(self.url, 'pwa').content['lighthouseResult']
             score = lighthouse_results['audits']['redirects-http']['score']
             is_criteria_met = True if score == 1 else False
             return self.get_criteria_object(score, is_criteria_met)
