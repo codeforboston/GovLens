64d386e22b4b6d805f2cd4f5ca28de4fffd0c5b9
djp9327 <djp9327@gmail.com>
2019-09-16 15:50:56 -0400

    changing static url

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -152,8 +152,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = 'apps/static/'
+STATIC_URL = '/apps/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'apps/static')
 
 GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY', '')
